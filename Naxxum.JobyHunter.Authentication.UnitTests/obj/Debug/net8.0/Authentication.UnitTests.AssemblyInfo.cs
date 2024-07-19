a93744a366b8ecb457e618939841748d291f7410
runningjack <amedora09@gmail.com>
2024-07-19 04:59:09 +0100

    fixed untracked files

--- Naxxum.JobyHunter.Authentication.UnitTests/obj/Debug/net8.0/Authentication.UnitTests.AssemblyInfo.cs
+++ Naxxum.JobyHunter.Authentication.UnitTests/obj/Debug/net8.0/Authentication.UnitTests.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Authentication.UnitTests")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+33e84dd324ee71630228a49a4b9e80461a99291a")]
 [assembly: System.Reflection.AssemblyProductAttribute("Authentication.UnitTests")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Authentication.UnitTests")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
